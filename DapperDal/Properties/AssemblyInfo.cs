e81584716dc791ae374b29fb11db2280330b923b
ArBing Xie <xiebingting@gmail.com>
2017-03-27 10:41:44 +0800

    打包DapperDal 1.5.2.1

--- DapperDal/Properties/AssemblyInfo.cs
+++ DapperDal/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
@@ -33,8 +32,8 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.2.0")]
-[assembly: AssemblyFileVersion("1.5.2.0")]
+[assembly: AssemblyVersion("1.5.2.1")]
+[assembly: AssemblyFileVersion("1.5.2.1")]
 [assembly: AssemblyInformationalVersion("1.5.2")]
 
 [assembly: CLSCompliant(true)]
