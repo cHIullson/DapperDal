a4be1c971158fe042762ede56adac5eaffbc8589
ArBing Xie <xiebingting@gmail.com>
2017-08-20 11:48:49 +0800

    [empty] initial commit

--- buildPackage.cmd
+++
@@ -1,3 +0,0 @@
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
-MSBuild src\DapperDal\DapperDal.csproj /t:Clean;Rebuild /p:Configuration=Release
-.nuget\nuget.exe pack DapperDal.nuspec /o releases
--- test/DapperDal.Test/IntegrationTests/SqlServer/Sql/TruncateTable.sql
+++
@@ -1,2 +0,0 @@
-﻿TRUNCATE TABLE Car;
-TRUNCATE TABLE Person;
