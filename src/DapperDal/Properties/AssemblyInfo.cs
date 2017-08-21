73520c6e4c33e0d34027d9b7d349cd165ec6a2df
ArBing Xie <xiebingting@gmail.com>
2017-08-21 08:18:26 +0800

    1.15.17

--- src/DapperDal/Properties/AssemblyInfo.cs
+++ src/DapperDal/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.16.0")]
-[assembly: AssemblyFileVersion("1.5.16.0")]
-[assembly: AssemblyInformationalVersion("1.5.16")]
+[assembly: AssemblyVersion("1.5.17.0")]
+[assembly: AssemblyFileVersion("1.5.17.0")]
+[assembly: AssemblyInformationalVersion("1.5.17")]
 
 [assembly: CLSCompliant(true)]
